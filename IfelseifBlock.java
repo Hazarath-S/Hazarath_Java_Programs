d7b6bcb6f360821d7110c9de8a2712dfc89a77ac
Hazarath -S <hazarathshaik286@gmail.com>
2024-05-05 12:46:13 +0530

    Dummy code

---
+++ IfelseifBlock.java
@@ -0,0 +1,28 @@
+package AssignmentPrograms;
+public class IfelseifBlock 
+{
+	public static void main(String[] args) 
+	{
+		int Operant1 = 200;
+		int Operant2 = 300;	
+		System.out.println("The System of given if Block is :");
+		
+		if(Operant1==Operant2) 
+		{
+			System.out.println("This is true");
+		}
+		else if(Operant1<Operant2) 
+		{
+			System.out.println("This is Automation");
+			
+			if(Operant1!=Operant2) 
+			{
+				System.out.println("9550075286");
+			}
+		}
+		else 
+		{
+			System.out.println("Method Overloading");
+		}	
+	}
+}
