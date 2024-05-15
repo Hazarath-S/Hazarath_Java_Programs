dcdc41757a08426acfb1ec670b1adbe4f1648728
Hazarath -S <hazarathshaik286@gmail.com>
2024-05-15 16:07:07 +0530

    My Collections

---
+++ Sort_Collections.java
@@ -0,0 +1,19 @@
+package Termination_Programs;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Sort_Collections 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList A1 = new ArrayList();
+		A1.add(21);
+		A1.add(854);
+		A1.add(251);
+		A1.add(85);
+		A1.add(450);
+		A1.add(84);
+		
+	   Collections.sort(A1);
+		System.out.println(A1);
+	}
+}
