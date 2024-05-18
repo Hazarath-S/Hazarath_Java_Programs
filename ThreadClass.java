23dd8a30395ad9eb751c454df3d0860ae9a1bc14
Hazarath -S <hazarathshaik286@gmail.com>
2024-05-18 20:10:03 +0530

    Push

---
+++ ThreadClass.java
@@ -0,0 +1,14 @@
+package methodsprograms;
+public class ThreadClass 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		for(int i = 1;i<=10;i++) 
+		{
+			System.out.println(i);
+			Thread.sleep(2000);	
+		}
+
+	}
+
+}
