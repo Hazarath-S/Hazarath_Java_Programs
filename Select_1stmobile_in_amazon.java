24f2b85c01072b19e414c58cf4075e696e583be6
Hazarath-S <168764583+Hazarath-S@users.noreply.github.com>
2024-06-04 19:45:49 +0530

    Update Select_1stmobile_in_amazon.java

--- Select_1stmobile_in_amazon.java
+++ Select_1stmobile_in_amazon.java
@@ -1,13 +1,10 @@
 package Sample_Programs;
-
 import java.util.Iterator;
 import java.util.Set;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Select_1stmobile_in_amazon 
 {
 	public static void main(String[] args) throws InterruptedException 
@@ -17,17 +14,14 @@ public class Select_1stmobile_in_amazon
          driver.manage().window().maximize();
          WebElement Search_Tf = driver.findElement(By.xpath("//input[@name ='q']"));
          Search_Tf.sendKeys("mobiles");
-         Search_Tf.sendKeys(Keys.ENTER);
-         
+         Search_Tf.sendKeys(Keys.ENTER);  
         WebElement firstshoe =  driver.findElement(By.className("_4WELSP"));
         firstshoe.click();
-        Thread.sleep(2000);
-        
+        Thread.sleep(2000);  
         Set<String> S1 =    driver.getWindowHandles();
         System.out.println(S1);
         Thread.sleep(2000);
-        driver.close();
-        
+        driver.close(); 
         Iterator<String> Parent_IDs =  S1.iterator();
         String Parent_ID = Parent_IDs.next();
          String Child_ID = Parent_IDs.next();  
